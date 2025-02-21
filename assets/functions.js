5b4d99284f0ed98df1303e5b358bfc63dbba33c8
Squirblechem <152367545+jman2476@users.noreply.github.com>
2025-02-21 15:24:28 -0500

    wrote function to list home directory

--- assets/functions.js
+++ assets/functions.js
@@ -1,4 +1,16 @@
 // import inquirer and other necessary tools
 const inquirer = require('@inquirer/prompts')
+const child_process = require('node:child_process')
 
 // write basic Bash script to open a folder
+async function write_bash(args) {
+    const ls = await child_process.spawn('ls', ['~/.'])
+    console.log('writing bash etc')
+    ls.stdout.on('data', (data) => {
+        console.log(`stdout: \n ${data}`)
+    })
+}
+
+module.exports = {
+    write_bash
+}
--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 const inquirer = require('@inquirer/prompts')
 
 // import functions and prompts
-
+const { write_bash } = require('./assets/functions')
 
 // initializing function
 async function init(){
     console.log('process initialized')
-    process.exit()
+    await write_bash()
+    //process.exit()
 }
 
 console.log('Welcome to Directory Copier, please follow the prompts to use the tool')
